f0111767d3c3b4f3dcbf0e90e337fa1efbb8cf0f
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-06-18 22:02:40 -0400

    add command line flags for listen address and port

--- cmd/go-to-aws/main.go
+++ cmd/go-to-aws/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/bikefrivolously/go-to-aws/internal/server"
 )
 
 func main() {
-	fmt.Println("Starting server...")
-	s := server.Server{}
+	var port = flag.Int("port", 8000, "Listen port for the web server.")
+	var address = flag.String("address", "localhost", "Listen address for the web server.")
+	flag.Parse()
+	fmt.Printf("Starting server on %s:%d...\n", *address, *port)
+	s := server.Server{Port: *port, Address: *address}
 	s.Run()
 }
